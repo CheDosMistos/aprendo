9dfeedba0e5c17770877e2677096a39e702daffb
Alejandro Mallo <100436765+CheDosMistos@users.noreply.github.com>
2026-08-24 09:32:43 +0200

    Render Aprendo through authenticated server runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://aprendo.molacomer.com',
+  output: 'server',
   trailingSlash: 'always',
   adapter: node({
     mode: 'standalone',
